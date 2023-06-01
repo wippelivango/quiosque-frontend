a66d19c5f3a31ea57dbb59caa0d888c0a831c7d7
Ivango Wippel <ivango.wippel@paytrack.com.br>
2023-06-01 07:08:46 -0300

    Criando layout base para as páginas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,26 @@
-import { Button } from '@mui/material';
+import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+
 import { useDrawerContext } from '../shared/contexts';
+import { Dashboard } from '../pages';
 
 export const AppRoutes = () => {
-  const { toggleDrawerOpen } = useDrawerContext();
+  const { setDrawerOptions } = useDrawerContext();
+
+  useEffect(() => {
+    setDrawerOptions([
+      {
+        icon: 'home',
+        path: '/pagina-inicial',
+        label: 'Página inicial',  
+      },
+    ]);
+  }, []);
 
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Toggle drawer</Button>} />
-
+      <Route path="/pagina-inicial" element={<Dashboard />} />
+      
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
