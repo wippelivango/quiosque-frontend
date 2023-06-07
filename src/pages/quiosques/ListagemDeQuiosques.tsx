b6f4c044ae225509969a23b3f4af9b2efdbcbcab
Ivango Wippel <ivango.wippel@paytrack.com.br>
2023-06-07 18:16:24 -0300

    Consultas desnecessárias com o useDebounce

--- src/pages/quiosques/ListagemDeQuiosques.tsx
+++ src/pages/quiosques/ListagemDeQuiosques.tsx
@@ -3,24 +3,29 @@ import { FerramentasDaListagem } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layouts";
 import { useEffect, useMemo } from "react";
 import { QuiosquesService } from "../../shared/services/api/quiosques/QuiosquesService";
+import { useDebounce } from "../../shared/hooks";
 
 
 export const ListagemDeQuiosques: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce(3000, false);
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(() => {
-    QuiosquesService.getAll(1, busca)
-      .then((result) => {
-        if (result instanceof Error) {
-          alert(result.message);
-        } else {
-          console.log(result);
-        }
-      });
+
+    debounce(() => {
+      QuiosquesService.getAll(1, busca)
+        .then((result) => {
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            console.log(result);
+          }
+        });
+    });
   }, [busca]);
 
   return (
---
+++ src/shared/hooks/UseDebounce.ts
@@ -0,0 +1,20 @@
+import { useCallback, useRef } from "react";
+
+export const useDebounce = (delay = 300, notDelayInFirstTime = true) => {
+  const debouncing = useRef<NodeJS.Timeout>();
+  const isFirsTime = useRef(notDelayInFirstTime);
+
+  const debounce = useCallback((func: () => void) => {
+    if (isFirsTime.current) {
+      isFirsTime.current = false;
+      func();
+    } else {
+      if (debouncing.current) {
+        clearTimeout(debouncing.current);
+      }
+      debouncing.current = setTimeout(() => func(), delay);
+    }
+  }, [delay]);
+
+  return { debounce };
+};
