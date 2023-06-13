a80410e6360066e421ebe4e93ad36239dba61fe7
Ivango Wippel <ivango.wippel@paytrack.com.br>
2023-06-13 20:52:34 -0300

    Salvando os dados do formulário no backend

--- src/pages/quiosques/DetalheDeQuiosques.tsx
+++ src/pages/quiosques/DetalheDeQuiosques.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { FormHandles } from "@unform/core";
 import { Form } from "@unform/web";
 
 import { LayoutBaseDePagina } from "../../shared/layouts";
@@ -7,10 +8,18 @@ import { FerramentasDeDetalhe } from "../../shared/components";
 import { QuiosquesService } from "../../shared/services/api/quiosques/QuiosquesService";
 import { VTextField } from "../../shared/forms";
 
+interface IFormData {
+  nome: string;
+  endereco: string;
+  cidade: string;
+}
+
 
 export const DetalheDeQuiosques: React.FC = () => {
   const { id = 'novo'} = useParams<'id'>();
   const navigate = useNavigate();
+
+  const formRef = useRef<FormHandles>(null);
 
   const [isLoading, setIsLoading] = useState(false);
   const [nome, setNome] = useState('');
@@ -28,15 +37,38 @@ export const DetalheDeQuiosques: React.FC = () => {
           navigate('/quiosques');
         } else {
           setNome(result.nome);
-          console.log(result);
+          formRef.current?.setData(result);
         }
       });
     }
 
   }, [id]);
 
-  const handleSave = () => {
-    console.log('Save');
+  const handleSave = (dados: IFormData) => {
+    setIsLoading(true);
+
+    if (id === 'novo') {
+      QuiosquesService
+      .create(dados)
+      .then((result) => {
+        setIsLoading(false);
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          navigate(`/quiosques/detalhe/${result}`);
+        }
+      });
+    } else {
+      QuiosquesService
+      .updateById(Number(id), { id: Number(id), ...dados })
+      .then((result) => {
+        setIsLoading(false);
+
+        if (result instanceof Error) {
+          alert(result.message);
+        }
+      });
+    }
   };
 
   const handleDelete = (id: number) => {
@@ -64,24 +96,19 @@ export const DetalheDeQuiosques: React.FC = () => {
           mostrarBotaoApagar={id !== 'novo'}
           mostrarBotaoNovo={id !== 'novo'}
 
-          aoClicarEmSalvar={handleSave}
-          aoClicarEmSalvarEFechar={handleSave}
+          aoClicarEmSalvar={() => formRef.current?.submitForm()}
+          aoClicarEmSalvarEFechar={() => formRef.current?.submitForm()}
           aoClicarEmApagar={() => handleDelete(Number(id))}
           aoClicarEmNovo={() => navigate('/quiosques/detalhe/novo')}
           aoClicarEmVoltar={() => navigate('/quiosques')}
         />
       }
     >
-      
-      <Form onSubmit={(dados) => console.log(dados)}>
-        
-        <VTextField
-          name="nome"
-        />
-
-        <button type="submit">Submit</button>
+      <Form ref={formRef} onSubmit={handleSave}>
+        <VTextField placeholder="Nome" name="nome" />
+        <VTextField placeholder="Endereço" name="endereco" />
+        <VTextField placeholder="Cidade" name="cidade" />
       </Form>
-
     </LayoutBaseDePagina>
   );
 };
