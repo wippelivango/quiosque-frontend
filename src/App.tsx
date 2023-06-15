b258f4ab8dcb60072f979a3f2d6ad35fc3d1d67e
Ivango Wippel <ivango.wippel@paytrack.com.br>
2023-06-14 22:57:52 -0300

    Traduzindo as mensagens de validação do Yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+
 import { AppThemeProvider, DrawerProvider } from './shared/contexts';
 import { MenuLateral } from "./shared/components";
+import './shared/forms/TraducoesYup';
 
 export const App = () => {
   return (
